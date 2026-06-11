29a7a1b3cabb0970205d66f7f296600bf412750f
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-11 22:07:56 +0800

    fix(command): make generation ref equality constexpr

--- KivoAudioCoreCpp/include/kivo/core/contract/command/identity/command_generation_ref.hpp
+++ KivoAudioCoreCpp/include/kivo/core/contract/command/identity/command_generation_ref.hpp
@@ -9,7 +9,7 @@ namespace kivo::core::contract {
 struct CommandGenerationRef {
     uint64_t generation{0};
     CommandOrigin origin{CommandOrigin::Unknown};
-    [[nodiscard]] bool operator==(const CommandGenerationRef&) const noexcept = default;
+    [[nodiscard]] constexpr bool operator==(const CommandGenerationRef&) const noexcept = default;
 };
 
 } // namespace kivo::core::contract
