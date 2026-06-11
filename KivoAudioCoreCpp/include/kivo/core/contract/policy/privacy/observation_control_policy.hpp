17c9ed8709713bbfeb252983c5ce23d8c1a4863e
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-11 21:14:00 +0800

    fix(policy): normalize observation control equality operator

--- KivoAudioCoreCpp/include/kivo/core/contract/policy/privacy/observation_control_policy.hpp
+++ KivoAudioCoreCpp/include/kivo/core/contract/policy/privacy/observation_control_policy.hpp
@@ -8,7 +8,7 @@ struct ObservationControlPolicy {
     uint8_t allow_trace_export{0};
     uint8_t allow_diagnostic_export{0};
 
-    friend bool operator==(const ObservationControlPolicy&, const ObservationControlPolicy&) noexcept = default;
+    [[nodiscard]] bool operator==(const ObservationControlPolicy&) const noexcept = default;
 };
 
 } // namespace kivo::core::contract::policy
