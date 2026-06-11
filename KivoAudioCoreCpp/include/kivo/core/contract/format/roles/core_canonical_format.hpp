4e982a049964190f86e4384b6515e5b37d7c62a6
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-11 23:05:08 +0800

    fix(format): make core canonical inspector constexpr

--- KivoAudioCoreCpp/include/kivo/core/contract/format/roles/core_canonical_format.hpp
+++ KivoAudioCoreCpp/include/kivo/core/contract/format/roles/core_canonical_format.hpp
@@ -23,7 +23,7 @@ struct CoreCanonicalFormat {
     AudioFormatDescriptor format{};
 
     // --- Validity ---
-    [[nodiscard]] bool is_valid() const noexcept {
+    [[nodiscard]] constexpr bool is_valid() const noexcept {
         return format.is_valid();
     }
 
