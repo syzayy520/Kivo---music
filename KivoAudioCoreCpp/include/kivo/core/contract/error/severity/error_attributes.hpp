632ef8b551f2fe10b1697e3349d42dc546701add
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-11 21:36:17 +0800

    fix(error): normalize attributes default initializers

--- KivoAudioCoreCpp/include/kivo/core/contract/error/severity/error_attributes.hpp
+++ KivoAudioCoreCpp/include/kivo/core/contract/error/severity/error_attributes.hpp
@@ -9,17 +9,17 @@
 namespace kivo::core::contract {
 
 struct ErrorAttributes {
-    ErrorSeverity severity = ErrorSeverity::Unknown;
-    bool recoverable = false;
-    bool retryable = false;
-    bool requires_reopen_device = false;
-    bool requires_rebuild_codec = false;
-    bool requires_user_action = false;
-    bool safe_to_continue = false;
-    RecoveryAction recovery_action = RecoveryAction::None;
-    bool host_notification = false;
-    bool metrics_event = false;
-    SafeCleanupPath safe_cleanup_path = SafeCleanupPath::None;
+    ErrorSeverity severity{ErrorSeverity::Unknown};
+    bool recoverable{false};
+    bool retryable{false};
+    bool requires_reopen_device{false};
+    bool requires_rebuild_codec{false};
+    bool requires_user_action{false};
+    bool safe_to_continue{false};
+    RecoveryAction recovery_action{RecoveryAction::None};
+    bool host_notification{false};
+    bool metrics_event{false};
+    SafeCleanupPath safe_cleanup_path{SafeCleanupPath::None};
 
     [[nodiscard]] bool operator==(const ErrorAttributes&) const noexcept = default;
 };
