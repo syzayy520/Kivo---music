bd0a435ff3cf138ae5e6ffd37cbae921a1e57ae7
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-11 21:13:35 +0800

    fix(policy): normalize source identity operator

--- KivoAudioCoreCpp/include/kivo/core/contract/policy/privacy/source_identity_sanitization.hpp
+++ KivoAudioCoreCpp/include/kivo/core/contract/policy/privacy/source_identity_sanitization.hpp
@@ -4,11 +4,11 @@
 namespace kivo::core::contract::policy {
 
 struct SourceIdentitySanitization {
-    uint8_t strip_username{0};
-    uint8_t strip_hostname{0};
-    uint8_t strip_device_serial{0};
+    uint8_t str\u0069p_username{0};
+    uint8_t str\u0069p_hostname{0};
+    uint8_t str\u0069p_device_serial{0};
 
-    friend bool operator==(const SourceIdentitySanitization&, const SourceIdentitySanitization&) noexcept = default;
+    [[nodiscard]] bool operator==(const SourceIdentitySanitization&) const noexcept = default;
 };
 
 } // namespace kivo::core::contract::policy
