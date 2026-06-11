694fbc35962a28b581dcf2bc05f574ffda9b1107
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-11 23:05:00 +0800

    fix(format): make native decoded inspector constexpr

--- KivoAudioCoreCpp/include/kivo/core/contract/format/roles/native_decoded_format.hpp
+++ KivoAudioCoreCpp/include/kivo/core/contract/format/roles/native_decoded_format.hpp
@@ -23,7 +23,7 @@ struct NativeDecodedFormat {
     AudioFormatDescriptor format{};
 
     // --- Validity ---
-    [[nodiscard]] bool is_valid() const noexcept {
+    [[nodiscard]] constexpr bool is_valid() const noexcept {
         return format.is_valid();
     }
 
