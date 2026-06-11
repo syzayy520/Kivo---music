3e5876e9aac0b80a1c46f7f68965cf71056043a4
syzayy520 <64846168+syzayy520@users.noreply.github.com>
2026-06-11 21:13:53 +0800

    fix(policy): repair source identity fields

--- KivoAudioCoreCpp/include/kivo/core/contract/policy/privacy/source_identity_sanitization.hpp
+++ KivoAudioCoreCpp/include/kivo/core/contract/policy/privacy/source_identity_sanitization.hpp
@@ -4,9 +4,9 @@
 namespace kivo::core::contract::policy {
 
 struct SourceIdentitySanitization {
-    uint8_t str\u0069p_username{0};
-    uint8_t str\u0069p_hostname{0};
-    uint8_t str\u0069p_device_serial{0};
+    uint8_t strip_username{0};
+    uint8_t strip_hostname{0};
+    uint8_t strip_device_serial{0};
 
     [[nodiscard]] bool operator==(const SourceIdentitySanitization&) const noexcept = default;
 };
